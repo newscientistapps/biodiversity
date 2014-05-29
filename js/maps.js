354253c60c22e6d13480c931f61b77e35b280f79
paldhous <peter@peteraldhous.com>
2014-05-28 19:07:11 -0700

    basic version

--- js/maps.js
+++ js/maps.js
@@ -1,28 +1,39 @@
-var amphib = L.mapbox.tileLayer('paldhous.amphib');
 
-var satellite = L.tileLayer.provider('MapQuestOpen.Aerial');
-
-var light = L.mapbox.tileLayer('mapbox.world-light');
+var amphib_all = L.mapbox.tileLayer('newscientist26102012.amphib_all'),
+	amphib_threat = L.mapbox.tileLayer('newscientist26102012.amphib_threat'),
+	birds_all = L.mapbox.tileLayer('newscientist26102012.birds_all'),
+	birds_threat = L.mapbox.tileLayer('newscientist26102012.birds_threat'),
+	mammals_all = L.mapbox.tileLayer('newscientist26102012.mammals_all'),
+	mammals_threat = L.mapbox.tileLayer('newscientist26102012.mammals_threat'),
+	boundaries = L.mapbox.tileLayer('newscientist26102012.boundaries'),
+	satellite = L.tileLayer.provider('MapQuestOpen.Aerial');
 	
 var map = L.map('map', {
     center: new L.LatLng(30, 0),
     zoom: 3, 
-    maxZoom: 7,
+    maxZoom: 5,
     minZoom: 2,
-    layers: [satellite, amphib] 
+    layers: [satellite, mammals_all, boundaries]  
 });
 
 var baseMaps = {
-    "Satellite": satellite,
-	"Map": light    
+	"All mammals": mammals_all,
+	"Threatened mammals": mammals_threat,
+	"All birds": birds_all,
+	"Threatened birds": birds_threat,
+	"All amphibians": amphib_all,
+	"Threatened amphibians": amphib_threat,	   
 };
 
+
 var overlayMaps = {
-    "Amphibian diversity": amphib
+    "National boundaries": boundaries
 };
 
 var layersControl = new L.Control.Layers(baseMaps, overlayMaps, {collapsed: false});
 map.addControl(layersControl);
+map.addControl(L.mapbox.legendControl());
+
 
 
 
